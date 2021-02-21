defd183f84179c5df459958e40884e8b13d7789a
udbhavpushkar <udbhavpushkar@gmail.com>
2021-02-21 23:14:05 +0530

    1st commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Button, Form, FormControl, Navbar} from "react-bootstrap";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Nav from "react-bootstrap/cjs/Nav";
-import logo from "../../logo.svg"
+import logo from "../../githublogo.svg"
 
 const Header = ({repo, owner, setOwner, setRepo, clickHandle}) => {
     const handleRepoChange = (e)=>{
@@ -14,20 +14,20 @@ const Header = ({repo, owner, setOwner, setRepo, clickHandle}) => {
 
     return (
         <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#">
+            <Navbar.Brand>
                 <img
                     alt=""
                     src={logo}
                     width="30"
                     height="30"
-                    className="d-inline-block align-top App-logo"
+                    className="d-inline-block align-top"
                 />{' '}
-                Github
+                Github Issue
             </Navbar.Brand>
             <Form style={{"flex": "auto"}} inline className="justify-content-end">
                 <FormControl onChange={handleOwnerChange} value={owner} type="text" placeholder="Owner Name" className="mr-sm-2" />
                 <FormControl onChange={handleRepoChange} value={repo} type="text" placeholder="Repository Name" className="mr-sm-2" />
-                <Button onClick={clickHandle} variant="outline-info">Search</Button>
+                <Button onClick={clickHandle} variant="info">Search</Button>
             </Form>
         </Navbar>
     );
